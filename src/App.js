e8cb07d21f3e40611e3ef7416208b86b53602363
SangwooPark-Sean <tkddn3367@gmail.com>
2021-02-03 17:50:01 +0900

    proptype check, childnodes 추가 예제

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
-import Test from './test';
+import PropTypes from "prop-types";
+
+function Stock({ent, price}) {
+  return <div>I bought {ent} when it was {price} won</div>;
+}
+
+const companies = [
+  {id:0, title:"apple", price:10}, 
+  {id:1, title:"tesla", price:8}, 
+  {id:2, title:"samsung", price:15}
+];
+
+Stock.propTypes = {
+  ent: PropTypes.string.isRequired,
+  price: PropTypes.number
+};
 
 function App() {
   return (
     <div>
-      hello
-      <Test />
+      {companies.map(x =>
+        <Stock key={x.id} ent={x.title} price={x.price}/>)}
     </div>
   );
 }
